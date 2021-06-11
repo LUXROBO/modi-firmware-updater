c41deddc52bec7cd416340bfba8979e7c8c19d26
Jinsung Ha <jsung5381@naver.com>
2021-06-11 18:08:01 +0900

    Notify user when turning interpreter off

--- modi_firmware_updater/core/esp32_updater.py
+++ modi_firmware_updater/core/esp32_updater.py
@@ -68,7 +68,7 @@ class ESP32FirmwareUpdater(serial.Serial):
     def update_firmware(self, force=False):
         # is_up_to_date = False
 
-        # Send packet to turn interpreter off
+        print('Turning interpreter off...")
         self.write(b'{"c":160,"s":0,"d":18,"b":"AAMAAAAA","l":6}')
 
         self.update_in_progress = True
